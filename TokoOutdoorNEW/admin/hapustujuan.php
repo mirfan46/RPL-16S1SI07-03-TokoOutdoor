d3924b848e9b054925b89583e516e83a014b4db0
enggarlukita <43667592+enggarlukita@users.noreply.github.com>
2019-01-14 22:13:51 +0700

    Delete hapustujuan.php

--- TokoOutdoorNEW/admin/hapustujuan.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $koneksi->query("DELETE FROM tb_tujuan WHERE id_tujuan='$_GET[id]'");
-    echo "<script>alert('alamat tujuan terhapus');</script>";
-    echo "<script>location='index.php?halaman=tujuan';</script>";
-?>
