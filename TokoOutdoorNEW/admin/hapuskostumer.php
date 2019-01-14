1ccec14d0eee3fdce0fea9d16a873d391d25be46
enggarlukita <43667592+enggarlukita@users.noreply.github.com>
2019-01-14 22:14:05 +0700

    Delete hapuskostumer.php

--- TokoOutdoorNEW/admin/hapuskostumer.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $koneksi->query("DELETE FROM tb_konsumen WHERE id_konsumen='$_GET[id]'");
-    echo "<script>alert('kostumer terhapus');</script>";
-    echo "<script>location='index.php?halaman=kostumer';</script>";
-?>
